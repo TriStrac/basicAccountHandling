fbdf4b86f75a74ba9572c60c0fbcecc3d95c78c6
TriStrac <borloloy777leigh@gmail.com>
2024-07-13 01:35:59 +0800

    added some more CORS config

--- src/main/java/com/busal/basicAccountHandling/BasicAccountHandlingApplication.java
+++ src/main/java/com/busal/basicAccountHandling/BasicAccountHandlingApplication.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
-
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,15 +17,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 @SpringBootApplication
 @EnableSwagger2
 @Configuration
 public class BasicAccountHandlingApplication {
 
-	@Bean
+    @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
@@ -36,26 +34,29 @@ public class BasicAccountHandlingApplication {
         return new CorsFilter(source);
     }
 
-	@Bean
-	public Docket api(){
-		return new Docket(DocumentationType.SWAGGER_2).select()
-		.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
-		.build().apiInfo(apiInfo()).useDefaultResponseMessages(false);
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+            .paths(PathSelectors.any())
+            .build()
+            .apiInfo(apiInfo())
+            .useDefaultResponseMessages(false);
+    }
 
-	@Bean
-	public ApiInfo apiInfo(){
-		final ApiInfoBuilder builder = new ApiInfoBuilder();
-		return builder.build();
-	}
+    @Bean
+    public ApiInfo apiInfo() {
+        final ApiInfoBuilder builder = new ApiInfoBuilder();
+        return builder.build();
+    }
 
-	@Bean
+    @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-	
-	public static void main(String[] args) {
-		SpringApplication.run(BasicAccountHandlingApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(BasicAccountHandlingApplication.class, args);
+    }
 }
+
